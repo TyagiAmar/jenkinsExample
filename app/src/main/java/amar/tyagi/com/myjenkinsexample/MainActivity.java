f64265a2534b86e7da22bb211fdf81e315213459
amar.tyagi <amar.tyagi@kelltonteck.com>
2017-02-03 18:00:36 +0530

    first

---
+++ app/src/main/java/amar/tyagi/com/myjenkinsexample/MainActivity.java
@@ -0,0 +1,16 @@
+package amar.tyagi.com.myjenkinsexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // tmp
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
