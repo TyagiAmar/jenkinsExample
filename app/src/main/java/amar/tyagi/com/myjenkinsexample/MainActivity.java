6faf350d5bf1d1939c497a31bbf780b625ddd029
amar.tyagi <amar.tyagi@kelltonteck.com>
2017-02-03 18:51:57 +0530

    jenknfile added

--- app/src/main/java/amar/tyagi/com/myjenkinsexample/MainActivity.java
+++ app/src/main/java/amar/tyagi/com/myjenkinsexample/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // tmp
         setContentView(R.layout.activity_main);
     }
 
